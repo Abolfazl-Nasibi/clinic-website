f0007a03a36f0aef942600174420299593fb760f
Abolfazl Nasibi <abolfazlnasibi2024@gmail.com>
2025-07-26 16:18:06 +0330

    vazir font added & body default font changed to vazir

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-primary'>nigger</div>
+      <Header />
     </>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,1 +1,12 @@
 import React from 'react'
+
+
+export default function Header(){
+
+    return <>
+        {/* header top */}
+        <div className=''>
+            
+        </div>
+    </>
+}
