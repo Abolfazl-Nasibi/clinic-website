835f8f1186cadb068f7b7fe1019ac7c8ea6f0d86
Abolfazl Nasibi <abolfazlnasibi2024@gmail.com>
2025-07-26 18:57:30 +0330

    main page head completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import MainPage from './components/main-page'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
       <Header />
+      <MainPage />
+      
     </>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,6 +3,7 @@ import { AiFillInstagram } from "react-icons/ai";
 import { FaTelegram } from "react-icons/fa6";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 export default function Header() {
   return (
@@ -33,38 +34,56 @@ export default function Header() {
       </div>
 
       {/* Header */}
-      <div className="flex justify-between items-center px-10 bg-primary h-16">
+      <div className="flex justify-between items-center px-10 bg-primary h-16 shadow-2xl z-50">
         {/* logo */}
         <img src="/logo.png" alt="logo" className="w-10 " />
         {/* links */}
         <ul className="flex justify-center items-center">
           <li>
-            <a href="#" className="text-secondary font-medium px-6 transition-all hover:text-secondary-light">
+            <a
+              href="#"
+              className="text-secondary font-medium px-6 transition-all hover:text-secondary-light"
+            >
               صفحه اصلی
             </a>
           </li>
           <li>
-            <a href="#" className="text-secondary font-medium px-6 transition-all hover:text-secondary-light">
+            <a
+              href="#"
+              className="text-secondary font-medium px-6 transition-all hover:text-secondary-light"
+            >
               محصولات
             </a>
           </li>
           <li>
-            <a href="#" className="text-secondary font-medium px-6 transition-all hover:text-secondary-light">
+            <a
+              href="#"
+              className="text-secondary font-medium px-6 transition-all hover:text-secondary-light"
+            >
               خدمات
             </a>
           </li>
           <li>
-            <a href="#" className="text-secondary font-medium px-6 transition-all hover:text-secondary-light">
+            <a
+              href="#"
+              className="text-secondary font-medium px-6 transition-all hover:text-secondary-light"
+            >
               مقالات
             </a>
           </li>
           <li>
-            <a href="#" className="text-secondary font-medium px-6 transition-all hover:text-secondary-light">
+            <a
+              href="#"
+              className="text-secondary font-medium px-6 transition-all hover:text-secondary-light"
+            >
               گالری عکس
             </a>
           </li>
           <li>
-            <a href="#" className="text-secondary font-medium px-6 transition-all hover:text-secondary-light">
+            <a
+              href="#"
+              className="text-secondary font-medium px-6 transition-all hover:text-secondary-light"
+            >
               درباره ما
             </a>
           </li>
@@ -72,11 +91,19 @@ export default function Header() {
         {/* login/sign in & shopping cart */}
         <div className="flex justify-center items-center gap-6">
           {/* shopping cart */}
-          <a href="#" className="text-secondary text-xl rounded-md p-2 transition-all hover:text-primary hover:bg-secondary">
+          <a
+            href="#"
+            className="text-secondary text-xl rounded-md p-2 transition-all hover:text-primary hover:bg-secondary"
+          >
             <FaCartShopping />
           </a>
           {/* login/signin */}
-          <a href="#" className="text-secondary font-light border py-2 px-6 rounded-md transition-all hover:bg-secondary hover:text-primary">ثبت نام</a>
+          <a
+            href="#"
+            className="text-secondary font-light border py-2 px-6 rounded-md transition-all hover:bg-secondary hover:text-primary"
+          >
+            ثبت نام
+          </a>
         </div>
       </div>
     </>
