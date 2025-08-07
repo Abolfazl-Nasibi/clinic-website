a780f9acf82880d1fe5cecd89d1a4b449cc3de27
Abolfazl Nasibi <abolfazlnasibi2024@gmail.com>
2025-08-07 08:22:32 +0330

    coded header ui & scrollY

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React , { useEffect, useState } from "react";
 import { FaLocationDot, FaT } from "react-icons/fa6";
 import { IoTime } from "react-icons/io5";
 import { RiInstagramFill } from "react-icons/ri";
 import { FaTelegram } from "react-icons/fa6";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <>
       {/* top information */}
-      <div className="flex justify-between items-center px-10 py-1 text-secondary bg-black">
+      <div className={`flex justify-between items-center px-10 py-1 text-secondary bg-black `}>
         {/* location & time reserve */}
         <div className="flex gap-4 font-medium text-sm">
           {/* location */}
@@ -33,6 +47,32 @@ export default function Header() {
           </a>
           <a href="#" target="__blank">
             <IoLogoWhatsapp />
+          </a>
+        </div>
+      </div>
+
+      {/* header */}
+      <div className={`flex items-center justify-between px-10 transition-all py-2 ${scrolled ? 'bg-white shadow-md fixed top-0 right-0 left-0 border-b-1' : ''}`}>
+        {/* logo */}
+        <img src="/logo-black.png" alt="" className="w-8" />
+        {/* pages */}
+        <ul className="flex items-center gap-10">
+          <li><a href="#" className="hover:border-b-2 transition-all border-b-2 border-white hover:border-black font-medium">صفحه اصلی</a></li>
+          <li><a href="#" className="hover:border-b-2 transition-all border-b-2 border-white hover:border-black font-medium">محصولات</a></li>
+          <li><a href="#" className="hover:border-b-2 transition-all border-b-2 border-white hover:border-black font-medium">خدمات</a></li>
+          <li><a href="#" className="hover:border-b-2 transition-all border-b-2 border-white hover:border-black font-medium">گالری</a></li>
+          <li><a href="#" className="hover:border-b-2 transition-all border-b-2 border-white hover:border-black font-medium">مقالات</a></li>
+          <li><a href="#" className="hover:border-b-2 transition-all border-b-2 border-white hover:border-black font-medium">درباره ما</a></li>
+        </ul>
+        {/* signIn/login & cart */}
+        <div className="flex items-center gap-2">
+          {/* cart */}
+          <a href="#">
+            < FaCartShopping />
+          </a>
+          {/* signIn/login */}
+          <a href="#" className="font-medium border-2 rounded-md px-4 py-1 hover:bg-black hover:text-white hover:border-black transition-all">
+            ثبت نام
           </a>
         </div>
       </div>
